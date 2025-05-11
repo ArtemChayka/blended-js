60c221ac40a6516dc2b9c446060d3b251412de50
Artem Chaychenko <drchaychenko@gmail.com>
2025-05-12 00:04:15 +0300

    except 11

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -86,3 +86,197 @@
 // console.log(stringArray.toSorted((a,b)=>a.localeCompare(b)));
 
 
+//===================================================================================================
+// Завдання 8:
+
+//  Відсортуйте масив об'єктів за віком у порядку зростання.
+// Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
+
+// const users = [
+//  { name: 'John', age: 27 },
+//  { name: 'Jane', age: 31 },
+//  { name: 'Bob', age: 19 },
+// ];
+
+// console.log(users.toSorted((a, b) => a.age - b.age)) // Зростання
+// console.log(users.toSorted((a, b) => b.age - a.age)) // Спадання
+
+
+//===================================================================================================
+// Завдання 9:
+
+// Дано масив об'єктів.
+// Створіть новий масив, що містить тільки об'єкти, в яких
+// вік більше 20 років.
+// Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
+
+// const user = [
+//  { name: 'John', age: 27 },
+//  { name: 'Jane', age: 31 },
+//  { name: 'Bob', age: 19 },
+// ]
+// console.log(user.filter(i=>i.age>20))
+
+
+//===================================================================================================
+// Завдання 10:
+
+// Дано масив чисел [1, 2, 3, 4, 5].
+// Застосуйте метод для обчислення суми елементів масиву.
+
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.reduce((acc,item)=>acc+item))            // без ПЕРШОГО параметру
+// console.log(numbers.reduce(((acc, item) => acc + item), 0))  // з ПЕРШИМ параметром
+
+
+//===================================================================================================
+// Завдання 11:
+
+// Розроби клас Calculator, який дозволяє виконувати арифметичні
+//  операції над числом за допомогою методів класу, підтримуючи
+// ланцюжковий виклик (method chaining).
+//
+// Вимоги до класу Calculator
+// - Метод number(value)
+// Встановлює початкове значення для наступних обчислень.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод getResult, Повертає поточний результат усіх операцій.
+// Не змінює значення, просто повертає його.
+
+// - метод add - Додає value до поточного значення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод substruct - Віднімає value від поточного значення. Повертає this.
+
+// - метод divide - Ділить поточне значення на value, якщо value не дорівнює 0.
+// Якщо value === 0, викидає помилку з повідомленням про неможливість ділення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+
+// - метод multiply -Множить поточне значення на value. Повертає this.
+
+// Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
+
+
+
+// class Calculator {
+//     number(value) {
+
+//         return this
+//     }
+// }
+
+
+// Приклад використання:
+// const calc = new Calculator();
+
+
+// const result = calc
+//   .number(10)   // Встановлюємо початкове значення 10
+//   .add(5)       // Додаємо 5 (10 + 5 = 15)
+//   .subtract(3)  // Віднімаємо 3 (15 - 3 = 12)
+//   .multiply(4)  // Множимо на 4 (12 * 4 = 48)
+//   .divide(2)    // Ділимо на 2 (48 / 2 = 24)
+//   .getResult(); // Отримуємо результат: 24
+
+
+//   console.log(result); // 24
+
+
+//===================================================================================================
+//Завдання 12:
+
+// Напиши клас Client який створює об'єкт з властивостями login email.
+// Оголоси приватні властивості #login #email, доступ до яких зроби
+// через геттер та сеттер login email
+
+// class Client {
+//     #login
+//     #email
+//     constructor(login,email) {
+//         this.#login = login,
+//             this.#email=email
+//     }
+//     get email() {
+//         return this.#email
+//     }
+//     set email(newEmail) {
+//         if (newEmail === '') {
+//             console.log('Має бути не пустий рядок');
+//         } else { this.#email = newEmail }
+//     }
+//     get login() {
+//         return this.#login
+//     }
+//     set login(newLogin) {
+//         if (newLogin === '') {
+//             console.log('Має бути не пустий рядок');
+//         } else { this.#login = newLogin }
+//     }
+// }
+// const user = new Client('doctor', 'drch@gmail.comm')
+// console.log(user.email);
+// console.log(user.login);
+// console.log(user.email = "mango@supermail.com");
+// console.log(user.login = "repetitor");
+// console.log(user.email);
+// console.log(user.login);
+
+
+//===================================================================================================
+//Завдання 13:
+
+//  Наслідування у класах!
+// Cтворіть клас `Person`, який містить наступні властивості:
+//  - `name` - ім'я людини;
+//  - `age`- вік людини;
+//  - `gender` - стать людини;
+//  - `email`- електронна пошта людини.
+
+// Крім того, клас `Person` має мати метод `getDetails()`,
+// який повертає об'єкт з ім'ям, віком, статтю
+//та електронною поштою людини.
+
+//
+// Потім Створіть клас `Employee`, який розширює клас `Person` і містить наступні властивості:
+//  - salary - зарплата співробітника;
+//  - department - відділ, в якому працює співробітник.
+// Крім того, клас `Employee` має мати метод `getEmployeeDetails()`, який повертає об'єкт з зарплатою співробітника та відділом, в якому він працює
+
+
+
+// class Person {
+//     constructor(obj) {
+//         this.name = obj.name,
+//             this.age = obj.age,
+//             this.gender = obj.gender,
+//             this.email = obj.email
+//     }
+//     getDetails() {
+//         return this
+//     }
+// }
+// class Employee extends Person {
+//     constructor(obj2, status) { // додали ще один аргумент окремо від обєкту
+//         super(obj2, status)
+//         this.salary = obj2.salary
+//         this.department = obj2.department
+//         this.status = status
+//     }
+//     getEmployeeDetails() {
+//         const obj3 = {}
+//         obj3.salary = this.salary
+//         obj3.department = this.department
+//         // console.log(this);
+//         return obj3
+//     }
+// }
+// const artem = new Person({ name: 'Artem', age: 34, gender: 'Male', email: 'drchay@gmail.com', lastName: 'Chaichenko', city: 'Kyiv' })
+// console.log(artem);
+// console.log(artem.getDetails());
+
+// const katya = new Employee({ salary: 340, department: 'Furnitur', lastName: 'Porokhivnyk', city: 'Vyshneve' }, 'wife') // super наслідує всі КЛЮЧІ від БАТЬКівського constructor() 
+// console.log(katya);
+// console.log(katya.getEmployeeDetails());
+
+
